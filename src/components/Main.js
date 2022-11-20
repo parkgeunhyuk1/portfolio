be65d8148468b582319cd5c947c560fd4775537b
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-11-20 17:48:56 +0900

    feature: Dark div

--- src/components/Main.js
+++ src/components/Main.js
@@ -106,6 +106,18 @@ const Center = styled.button`
   }
 `;
 
+const DarkDiv = styled.div`
+position: absolute;
+top: 0;
+background-color: #000;
+bottom: 0;
+right: 50%;
+width: ${props => props.click ? '50%' : '0%'};
+height: ${props => props.click ? '100%' : '0%'};
+z-index:1;
+transition: height 0.5s ease, width 1s ease 0.5s;
+`
+
 const Main = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => {
@@ -113,6 +125,7 @@ const Main = () => {
   };
   return (
     <MainContainer>
+        <DarkDiv click={click}/>
       <Container>
         <PowerButton />
         <LogoComponent />
