adb397b945e7c3e5f9b1085ae8575980c161f7c9
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-11-20 23:09:25 +0900

    Feature: BlogPage CardGrid

---
+++ src/subComponents/AnChor.js
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+import { Link } from '../components/AllSvgs'
+
+const Container=styled.div`
+position: relative;
+`
+const Slider=styled.div`
+position: fixed;
+top: 0;
+right: 2rem;
+`
+
+const AnChorComponent = () => {
+
+    const ref=useRef(null);
+    const hiddenRef=useRef(null);
+
+  return (
+    <Container>
+      <Slider ref={ref}>
+        {
+            [...Array(25)].map((x,id)=>{
+                return <Link key={id} width={25} height={25} fill='currentColor' className='chain' />
+            })
+        }
+      </Slider>
+    </Container>
+  )
+}
+
+export default AnChorComponent;
