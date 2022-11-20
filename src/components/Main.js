ff6450ee10b95b3e25678cb07b653d9a1cfcab07
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-11-20 22:14:39 +0900

    Feature: mainpage

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
 import { YinYang } from "./AllSvgs";
 import Intro from "./Intro";
-
+import { motion } from "framer-motion";
 const MainContainer = styled.div`
   background: ${(props) => props.theme.body};
   width: 100vw;
@@ -144,20 +144,35 @@ const Main = () => {
           target="_blank"
           to={{ pathname: "matilto:rmsgur95@gmail.com" }}
         >
-          <h2>Say hi..</h2>
+          <motion.h2
+           whileHover={{scale: 1.1}}
+           whileTap={{scale: 0.9}}
+          >Say hi..</motion.h2>
         </Contact>
         <Blog to="/blog">
-          <h2>Blog</h2>
+          <motion.h2
+           whileHover={{scale: 1.1}}
+           whileTap={{scale: 0.9}}
+          >Blog</motion.h2>
         </Blog>
         <Work to="/work" click={click}>
-          <h2>Work</h2>
+          <motion.h2
+           whileHover={{scale: 1.1}}
+           whileTap={{scale: 0.9}}
+          >Work</motion.h2>
         </Work>
         <BottomBar>
           <About to="/about" click={click}>
-            <h2>About.</h2>
+            <motion.h2
+             whileHover={{scale: 1.1}}
+             whileTap={{scale: 0.9}}
+            >About.</motion.h2>
           </About>
           <Skills to="/skills" click={click}>
-            <h2>My Skills.</h2>
+            <motion.h2
+             whileHover={{scale: 1.1}}
+             whileTap={{scale: 0.9}}
+            >My Skills.</motion.h2>
           </Skills>
         </BottomBar>
       </Container>
