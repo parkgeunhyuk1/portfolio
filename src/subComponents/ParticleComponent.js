55f5cd4e1b686858efeb4416c4ab388fd563b1b6
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-11-21 10:52:40 +0900

    Fix: Particle Error

--- src/subComponents/ParticleComponent.js
+++ src/subComponents/ParticleComponent.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import Particles from "react-tsparticles";
 
+import { loadFull } from "tsparticles";
 // particle config files
 import configLight from "../config/particlesjs-config-light.json"
 import configDark from "../config/particlesjs-config.json"
-
 const Box = styled.div`
   position: absolute;
   top: 0;
@@ -13,16 +13,28 @@ const Box = styled.div`
   bottom: 0;
   z-index: 0;
 `
-
-
 const ParticleComponent = (props) => {
-
+    const particlesInit = async (main) => {
+        console.log(main);
+     
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(main);
+      };
+      const particlesLoaded = (container) => {
+        console.log(container);
+      };
 
     return (
         <Box>
             <Particles
+             init={particlesInit}
+             loaded={particlesLoaded}
                 style={{position: "absolute", top: 0}}
-                params={props.theme === "light" ? configLight : configDark}/>
+                params={props.theme === "light" ? configLight : configDark}
+                
+                />
         </Box>
     );
 };
